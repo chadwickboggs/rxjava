01e7325f29774e14b1224842ec8e5dd2d575e2ca
Tiffany Timbric <tiffanytimbric@gmail.com>
2023-01-25 05:12:33 -0700

    Added Javadoc describing the Data Flow Types supported by the Reactive eXtension API.

--- src/main/java/com/tiffanytimbric/rxjava/Main.java
+++ src/main/java/com/tiffanytimbric/rxjava/Main.java
@@ -5,14 +5,40 @@ import rx.Single;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 
+/**
+ * The Reactive eXtensions API supports two methods of data flow, push and pull.
+ * The Java Streams API support supports only one method of data flow, pull.
+ * <h3>Data Flow Types</h3>
+ * <ul>
+ *     <li>Push - New data arrives asynchronously until the publisher signals its end.
+ *                The subscriber process the incoming data as it arrives.  This flow
+ *                type is alloigous to event stream, event processing.</li>
+ *     <li>Pull - A fixed block of data pre-exists before the processing of it begins.
+ *                This flow type is analigous to Java Collections.</li>
+ * </ul>
+ */
 public final class Main {
 
     public static void main( @Nullable String... args ) {
-        System.out.println("Program Start");
+        System.out.println("Program Start\n");
 
+        System.out.println("Pulling data...");
+        List<Integer> allNumbers = pullTheData();
+        System.out.println( "All Numbers: " + allNumbers );
+
+        System.out.println("\nPushing data...");
+        allNumbers = pushTheData();
+        System.out.println( "All Numbers: " + allNumbers );
+
+        System.out.println("\nProgram End");
+    }
+
+    @Nonnull
+    private static List<Integer> pullTheData() {
         final Single<Integer> zeroSingle = Single.just( 0 );
         final Observable<Integer> oddNumbersObs = Observable.just( 1, 3, 5, 7 );
         final Observable<Integer> evenNumbersObs = Observable.just( 2, 4, 6, 8 );
@@ -23,13 +49,15 @@ public final class Main {
             .mergeWith( evenNumbersObs )
 //            .subscribe( System.out::println )
             .toList();
-        final List<Integer> allNumbers = allNumbersObs
+
+        return allNumbersObs
             .toBlocking()
             .singleOrDefault( new ArrayList<>() );
+    }
 
-        System.out.println( "All Numbers: " + allNumbers );
-
-        System.out.println("Program End");
+    @Nonnull
+    private static List<Integer> pushTheData() {
+        return new ArrayList<>();
     }
 
 }
