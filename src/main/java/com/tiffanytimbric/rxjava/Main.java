16e5e1e95b835badf1e0e8d18a91d56c19bd1054
Tiffany Timbric <tiffanytimbric@gmail.com>
2023-01-25 13:00:27 -0700

    Distinguished blocking read of Observable data from subscription read of Observable data.

--- src/main/java/com/tiffanytimbric/rxjava/Main.java
+++ src/main/java/com/tiffanytimbric/rxjava/Main.java
@@ -3,10 +3,10 @@ package com.tiffanytimbric.rxjava;
 import rx.Observable;
 import rx.Single;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -20,14 +20,14 @@ import javax.annotation.Nullable;
  *         <blockquote>
  *          New data arrives asynchronously until the publisher signals its end.
  *          The subscriber process the incoming data as it arrives.  This flow
- *          type is alloigous to event stream, event processing.
+ *          type is analogous to event stream, event processing.
  *         </blockquote>
  *     </li>
  *     <li>
  *         <b>Pull:</b>
  *         <blockquote>
  *          A fixed block of data pre-exists before the processing of it begins.
- *          This flow type is analigous to Java Collections.
+ *          This flow type is analogous to Java Collections.
  *         </blockquote>
  *     </li>
  * </ul>
@@ -35,40 +35,50 @@ import javax.annotation.Nullable;
 public final class Main {
 
     public static void main( @Nullable String... args ) {
-        System.out.println("Program Start\n");
+        System.out.println( "Program Start\n" );
 
-        System.out.println("Pulling data...");
-        List<Integer> allNumbers = pullTheData();
+        System.out.println( "Pulling data..." );
+        Observable<Integer> allNumbersObs = createPullTheDataObs();
+        System.out.println( "Extracting data as list from Pull Data Flux..." );
+        List<Integer> allNumbers = extractAsList( allNumbersObs );
         System.out.println( "All Numbers: " + allNumbers );
 
-        System.out.println("\nPushing data...");
-        allNumbers = pushTheData();
-        System.out.println( "All Numbers: " + allNumbers );
+        System.out.println( "Pulling data..." );
+        allNumbersObs = createPullTheDataObs();
+        System.out.println( "Subscribing to Pull Data Flux..." );
+        allNumbersObs.subscribe( System.out::println );
 
-        System.out.println("\nProgram End");
+        System.out.println( "\nPushing data..." );
+        allNumbersObs = createPushTheDataObs();
+        allNumbersObs.subscribe( System.out::println );
+
+        System.out.println( "\nProgram End" );
     }
 
     @Nonnull
-    private static List<Integer> pullTheData() {
+    private static List<Integer> extractAsList(
+            @Nonnull final Observable<Integer> allNumbersObs
+    ) {
+        return allNumbersObs
+                .toList()
+                .toBlocking()
+                .singleOrDefault( new ArrayList<>() );
+    }
+
+    @Nonnull
+    private static Observable<Integer> createPullTheDataObs() {
         final Single<Integer> zeroSingle = Single.just( 0 );
         final Observable<Integer> oddNumbersObs = Observable.just( 1, 3, 5, 7 );
         final Observable<Integer> evenNumbersObs = Observable.just( 2, 4, 6, 8 );
 
-        final Observable<List<Integer>> allNumbersObs = zeroSingle
-            .toObservable()
-            .mergeWith( oddNumbersObs )
-            .mergeWith( evenNumbersObs )
-//            .subscribe( System.out::println )
-            .toList();
-
-        return allNumbersObs
-            .toBlocking()
-            .singleOrDefault( new ArrayList<>() );
+        return zeroSingle.toObservable()
+                .mergeWith( oddNumbersObs )
+                .mergeWith( evenNumbersObs );
     }
 
     @Nonnull
-    private static List<Integer> pushTheData() {
-        return new ArrayList<>();
+    private static Observable<Integer> createPushTheDataObs() {
+        return Observable.empty();
     }
 
 }
